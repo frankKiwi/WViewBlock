f968b8225313fa2ef4b35af084b13d353575954e
fanrenFRank <1778907544@qq.com>
2018-06-26 16:57:58 +0800

    no message

--- WViewBlockKit/WViewKit.framework/Headers/WViewKit.h
+++ WViewBlockKit/WViewKit.framework/Headers/WViewKit.h
@@ -1,21 +1,12 @@
 //
-//  WViewBlock.h
-//  WViewBlock
+//  WViewKit.h
+//  WViewKit
 //
 //  Created by LWW on 2018/6/26.
 //  Copyright © 2018年 LWW. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
-
-//! Project version number for WViewBlock.
-FOUNDATION_EXPORT double WViewBlockVersionNumber;
-
-//! Project version string for WViewBlock.
-FOUNDATION_EXPORT const unsigned char WViewBlockVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <WViewBlock/PublicHeader.h>
-
 #import "UIButton+Kiwi.h"
 #import "UICollectionView+SetIPhoneX.h"
 #import "UIImageView+Kiwi.h"
@@ -30,5 +21,4 @@ FOUNDATION_EXPORT const unsigned char WViewBlockVersionString[];
 #import "TableViewAnimationKit.h"
 #import "UITableView+XSAnimationKit.h"
 #import "TableViewAnimationKitConfig.h"
-
 
